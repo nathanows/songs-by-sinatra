99fa198df860ea2c31a09ce1b8164189afe919a5
Nathan Owsiany <nowsiany@gmail.com>
2014-12-11 21:04:01 -0700

    initial commit

---
+++ main.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'slim'
+require 'sass'
+require './song'
+require 'sinatra/reloader' if development?
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  @title = "Get In Contact"
+  slim :contact
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
+end
+
+not_found do
+  slim :not_found
+end
