1f1170515f7bb4d20c3786da8585db04d2893089
Nathan Owsiany <nowsiany@gmail.com>
2014-12-11 22:08:52 -0700

    added some helpers

--- main.rb
+++ main.rb
@@ -12,6 +12,18 @@ configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
+helpers do
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+    end.join
+  end
+
+  def current?(path='/')
+    (request.path==path || request.path==path+'/') ? "current" : nil
+  end
+end
+
 configure do
   enable :sessions
   set :username, 'frank'
